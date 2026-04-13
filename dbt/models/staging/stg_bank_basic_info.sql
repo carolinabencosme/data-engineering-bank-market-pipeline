d133500e3f9d5a8be5f016b88a9f995bc86dcdb2
Carolina <cbencome@gmail.com>
2026-04-12 21:06:58 -0400

    Add staging model for bank basic information extraction

---
+++ dbt/models/staging/stg_bank_basic_info.sql
@@ -0,0 +1,20 @@
+select
+    symbol,
+    snapshot_date,
+    company_name,
+    industry,
+    sector,
+    employee_count,
+    city,
+    phone,
+    state,
+    country,
+    website,
+    address,
+    market_cap,
+    currency,
+    exchange,
+    ingested_at,
+    source_system,
+    batch_id
+from {{ source('landing', 'bank_basic_info') }}
